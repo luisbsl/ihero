8218bfde5968f7b978751e09028d232a5fa3be9d
Luis Ismael <luisbsl@gmail.com>
2018-05-17 03:04:52 -0300

    setting storybook

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import StorybookUI from './storybook'
 import { AppRegistry, YellowBox } from 'react-native'
 
 import App from './App'
@@ -8,4 +9,4 @@ YellowBox.ignoreWarnings([
   'The StackNavigator function name is deprecated, please use createStackNavigator instead'
 ])
 
-AppRegistry.registerComponent('ihero', () => App)
+AppRegistry.registerComponent('ihero', () => StorybookUI)
